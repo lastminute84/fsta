6df2a3246aa8e7bc14ebd5abe5004b9cded88d12
mac <mac@macs-iMac.local>
2019-07-16 22:26:14 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import HighlightedText from './components/HighlightedText'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    animationStarted: false
+  }
+
+  startButtonClickHandler = () => {
+    console.log('start button was clicked')
+    this.setState({ animationStarted: true })
+  }
+
+  render() {
+    return (
+      <>
+        <HighlightedText started={this.state.animationStarted}>
+          Enim laborum id laboris officia ipsum ut cupidatat anim ex pariatur
+          reprehenderit Lorem aliqua excepteur. Mollit tempor magna in nisi anim
+          sunt commodo qui proident id aliquip enim fugiat ex. Laborum laborum
+          reprehenderit aute consequat enim. Eu id excepteur culpa id deserunt
+          ad cupidatat sunt non aute quis aliquip Lorem. Laborum esse commodo
+          sint officia do aute non pariatur. Nostrud est consectetur quis
+          eiusmod eiusmod nisi exercitation irure nulla id.
+        </HighlightedText>
+        <button onClick={this.startButtonClickHandler}>Start</button>
+      </>
+    )
+  }
 }
-
-export default App;
