798cd1b5686e6ccf4cf34903cb097f6504282c12
Csaba Farkas <csabafarkas17@gmail.com>
2019-07-20 11:47:39 +0100

    git config

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 
 export default function Controls(props) {
-  const [started, setStarted] = useState(0)
-
   return (
     <>
       <button className="start-btn" onClick={props.startButtonClicked}>
